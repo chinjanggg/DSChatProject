b5619ce0f2fe6196290b672c906c26d2fce96084
chinjanggg <ice_sripaiboon@hotmail.com>
2018-04-28 18:20:31 +0700

    clear input box when close

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,4 +1,3 @@
-
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -10,17 +9,19 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
-modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-modal.style.display = "none";
+  document.getElementById("friend-username").value = "";
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-if (event.target == modal) {
-modal.style.display = "none";
-}
+  if (event.target == modal) {
+    document.getElementById("friend-username").value = "";
+    modal.style.display = "none";
+  }
 }
