03248520d7569b56e52d444364b8b6465db9ad74
chinjanggg <ice_sripaiboon@hotmail.com>
2018-04-29 12:56:42 +0700

    create chat add friend modal

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,11 +1,12 @@
 // Get the modal
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("addFriend");
-
-// Get the <span> element that closes the modal
+var btn = document.getElementById("createChat");
 var span = document.getElementsByClassName("close")[0];
+var addbtn = document.getElementById("btn-addfriend");
+var chatname = document.getElementById("name-input");
+var funame = document.getElementById("funame-input");
+var friendlist = document.getElementById("friendlist");
+var fldiv = document.getElementsByClassName("friendlistdiv")
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
@@ -14,14 +15,32 @@ btn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  document.getElementById("friend-username").value = "";
+  chatname.value = "";
+  funame.value = "";
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    document.getElementById("friend-username").value = "";
+    chatname.value = "";
+    funame.value = "";
     modal.style.display = "none";
   }
+}
+
+addbtn.onclick = function() {
+  if(funame.value) {
+    addFriendList(funame.value);
+    funame.value = "";
+    fldiv.scrollTop = fldiv.scrollHeight - fldiv.clientHeight;
+  }
+}
+
+function addFriendList(username) {
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode(username);
+  node.appendChild(textnode);
+  friendlist.append(node);
+  console.log("add")
 }
