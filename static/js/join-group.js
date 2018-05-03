74ff3f0d8b0fb7b7ece34059de9f7c2d0fa01d4b
chinjanggg <ice_sripaiboon@hotmail.com>
2018-05-03 21:53:37 +0700

    socket.emit

--- static/js/join-group.js
+++ static/js/join-group.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+  var socket = io.connect('http://localhost:5000');
+
   $("#join-group").on('click', function() {
-    emit('join', $("#gid-input").val());
+    socket.emit('join', $("#gid-input").val());
     console.log('join group id: '+$("#gid-input").val());
     $("#gid-input").val("");
   });
