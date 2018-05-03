baaf1080ae6ec828db62e0c0e42550dfd252a905
chinjanggg <ice_sripaiboon@hotmail.com>
2018-05-03 19:07:00 +0700

    create group form

--- static/js/group-modal.js
+++ static/js/group-modal.js
@@ -2,45 +2,17 @@
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("createChat");
 var span = document.getElementsByClassName("close")[0];
-var addbtn = document.getElementById("btn-addfriend");
-var chatname = document.getElementById("name-input");
-var funame = document.getElementById("funame-input");
-var friendlist = document.getElementById("friendlist");
-var fldiv = document.getElementsByClassName("friendlistdiv")
 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
 span.onclick = function() {
-  closeclear();
+  modal.style.display = "none";
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
-    closeclear();
+    modal.style.display = "none";
   }
-}
-
-function closeclear() {
-  chatname.value = "";
-  funame.value = "";
-  modal.style.display = "none";
-  $("#friendlist").empty();
-}
-
-addbtn.onclick = function() {
-  if(funame.value) {
-    addFriendList(funame.value);
-    funame.value = "";
-    fldiv.scrollTop = fldiv.scrollHeight - fldiv.clientHeight;
-  }
-}
-
-function addFriendList(username) {
-  var node = document.createElement("LI");
-  var textnode = document.createTextNode(username);
-  node.appendChild(textnode);
-  friendlist.append(node);
-  console.log("add "+username)
 }
