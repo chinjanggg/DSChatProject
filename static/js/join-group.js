71604621e35900e26b380142f7053a384bf1d771
chinjanggg <ice_sripaiboon@hotmail.com>
2018-05-03 22:06:46 +0700

    emit again + update html to-do list

--- static/js/join-group.js
+++ static/js/join-group.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var socket = io.connect('http://localhost:5000');
 
   $("#join-group").on('click', function() {
-    socket.emit('join', $("#gid-input").val());
+    socket.emit('join', {'group': $("#gid-input").val()});
     console.log('join group id: '+$("#gid-input").val());
     $("#gid-input").val("");
   });
