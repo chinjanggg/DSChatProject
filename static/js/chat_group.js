312053cba1798c72b1e7193dd83ed45349d6ba17
aomrawissada <aompk105@gmail.com>
2018-05-04 13:15:42 +0700

    bootstrap

--- static/js/chat_group.js
+++ static/js/chat_group.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	var socket = io.connect('http://localhost:5000');
 
-  $("#chat-list-item").on('click', function() {
+  $('.chat-list-item').on('click', function() {
+  	console.log('loading...' + this.id);
     socket.emit('switch', {'group': this.id});
-    console.log('load chat group id: '+ this.id);
+    console.log('loaded chat group id: '+ this.id);
   });
+
 });
