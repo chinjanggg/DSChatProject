e65dde71a7a21ebe677cf416915b9948d93610c7
pcharuthamrong <p.charuthamrong@gmail.com>
2018-05-04 17:46:41 +0700

    bug fix

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def on_break():
 def send_message(message, user, time, group):
 	time = time.replace(microsecond=0).isoformat()
 	#socketio.emit('text', {'message':message, 'user':user, 'time':time}, broadcast=True, room=group)
-	socketio.emit('text', {'message':message, 'user':user, 'time':time}, broadcast=True)
+	socketio.emit('text', {'message':message, 'user':user, 'time':time, 'group':group}, broadcast=True)
 
 @socketio.on('message')
 def handle_message(message):
